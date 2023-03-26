888dab13946d26a111741859cc39d89b0f55c98a
hanzn-zzx <128143343+hanzn-zzx@users.noreply.github.com>
2023-03-26 08:47:07 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,13 +16,13 @@ module.exports = {
           ],
         sidebar: 
         [
-          ['/', '说明'],
+          ['/', '文档说明'],
          {
-            title: 'deepin 常用资源整理',   // 必要的
-            path: '/deepin-src-col/sec-0',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            title: 'deepin 常用资源整理',   
             collapsable: false, // 可选的, 默认值是 true,
             sidebarDepth: 2,    // 可选的, 默认值是 1
             children: [
+               ['/deepin-src-col/sec-0', '说明'],
                ['/deepin-src-col/sec-1', '一、综合'],
                ['/deepin-src-col/sec-2', '二、系统'],
                ['/deepin-src-col/sec-3', '三、硬件和设备'],
